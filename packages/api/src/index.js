bb0f188d85b129c428ffddf50354c4f7c8cfab57
Cosmin Serdean <cosmoserdean@gmail.com>
2019-09-02 19:49:03 +0300

    Add sentry logging

--- packages/api/src/index.js
+++ packages/api/src/index.js
@@ -1,6 +1,8 @@
-const {ApolloServer} = require('apollo-server')
+const { ApolloServer } = require('apollo-server')
+const Sentry = require('@sentry/node')
 
 require('dotenv').config()
+Sentry.init({ dsn: process.env.GEM_SENTRY_DSN })
 
 require('./db')
 
@@ -8,22 +10,26 @@ const production = process.env.NODE_ENV === 'production'
 
 const typeDefs = require('./schema')
 const resolvers = require('./resolvers')
-const {getViewer} = require('./utils')
+const { getViewer } = require('./utils')
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   introspection: !production,
   playground: !production,
-  context: async ({req}) => {
+  formatError: error => {
+    Sentry.captureMessage(error.message)
+    return error
+  },
+  context: async ({ req }) => {
     const token = req.headers.authorization
 
     if (!token) return {}
 
-    return {viewer: await getViewer(token)}
-  },
+    return { viewer: await getViewer(token) }
+  }
 })
 
-server.listen().then(({url}) => {
+server.listen().then(({ url }) => {
   console.log(`🚀 Server ready at ${url}`)
 })
